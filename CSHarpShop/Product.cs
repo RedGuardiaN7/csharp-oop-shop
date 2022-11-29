1d9f3b286db45419426e045f1f33c7cb7a831628
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-29 14:18:56 +0100

    Aggiunte caratterisitche base al prodotto

---
+++ CSHarpShop/Product.cs
@@ -0,0 +1,30 @@
+﻿//CONSEGNA: Nel progetto CSharpShop , creare la classe Prodotto che gestisce i prodotti del vostro shop.
+//Caratteristiche: codice, nome, descrizione, prezzo, iva
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHarpShop
+{
+    public class Product
+    {
+
+    //Viene utilizzato "public" ma assieme ai setter e getter auto-implementati, che rendono le caratteristiche private, ma peremttono di usare i getter e i setter.
+        public int code { get; set; }
+
+        public string name { get; }
+
+        public string description { get; set; }
+
+        public int price { get; set; }
+
+        public int iva { get; set; }
+
+
+
+
+    }
+}
--- CSHarpShop/Program.cs
+++ CSHarpShop/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//CONSEGNA: usando la classe Product e i livelli di accesso (public, private), i costruttori, i metodi getter e setter, fare sì che:
+// - Alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random.
+// - Il codice prodotto sia accessibile solo in lettura
+// - Gli altri attributi siano accessibili sia in lettura che in scrittura
+
+using CSHarpShop;
+
+Random GeneratorRandomNumber = new Random();
+
+int RandomNumber = GeneratorRandomNumber.Next(0, 99999999);
+
+Product product_1 = new Product();
+
+product_1.name = "Prova!";
+
+Console.WriteLine(product_1.name);
